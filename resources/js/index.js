4ed28eddf02437b5cdb90d498c1784a4726f1d81
rex <neverlo@live.cn>
2016-05-06 02:23:41 +0800

    upload files

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,11 +2,21 @@ $(function(){
     $("#pm-nav li a").click(function(){//作用域this,表示当前点击的对象
         //attr获取this(a)的属性comp的值
         var compEle = $(this).attr("comp"); //jquery先获取某个html对象，然后再做某个事情
-        if(typeof(compEle)=="undefined" || compEle==""){
-            return;
-        }
         var htmlPath = compEle + ".html";
         var title = $(this).find("span").html();
+        if(typeof(compEle)=="undefined" || compEle==""){
+            if(title=="项目资料"){//展开菜单
+                $("#dblist").hide();
+                $("#projectlist").show();
+            }else if(title=="数据&amp;数据库"){
+                $("#projectlist").hide();
+                $("#dblist").show();
+            }
+            return;
+        }else if(compEle=="log" || compEle=="introduce"){
+            $("#dblist").hide();
+            $("#projectlist").hide();
+        }
         $("#childTitle").html(title);
         var pageh = $(this).attr("pageh");
         $("#swan").css({"height":pageh});
@@ -19,4 +29,22 @@ function showProfile(){
     $("#childTitle").html("个人资料");;
     $("#swan").css({"height":"651px"});
     $("#swan").attr("src","profile.html");
+}
+
+//显示系统消息
+function showMsg(){
+    if($("#msg").is(":hidden")){
+        $("#msg").show();
+    }else{
+        $("#msg").hide();
+    }
+}
+
+//显示个人操作面板
+function showUser(){
+    if($("#user").is(":hidden")){
+        $("#user").show();
+    }else{
+        $("#user").hide();
+    }
 }
